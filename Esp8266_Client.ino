f681f23f03c35535d54f2721aad74661367e92c1
sagesmither <haphonggoroki@gmail.com>
2023-05-04 12:03:30 +0700

    done

--- Esp8266_Client.ino
+++ Esp8266_Client.ino
@@ -18,6 +18,8 @@
 
 #include <ArduinoWebsockets.h>
 #include <ESP8266WiFi.h>
+#include <Wire.h> 
+#include "rgb_lcd.h"
 
 const char* ssid = "PhongPhuong"; //Enter SSID
 const char* password = "12345678900"; //Enter Password
@@ -29,62 +31,180 @@ using namespace websockets;
 
 WebsocketsClient client;
 
-int giatri;
-int camBienRung = 16;
+//lcd
+rgb_lcd lcd;
+
+const int colorR = 0;
+const int colorG = 0;
+const int colorB = 255;
+
 void setup() {
-    Serial.begin(9600);
+  Serial.begin(9600);
 
-    //pin led
-    pinMode(camBienRung, INPUT);
-    pinMode(5, OUTPUT);
-    // Connect to wifi
-    // WiFi.begin(ssid, password);
+  // lcd.begin(16, 2);
+  // lcd.setRGB(colorR, colorG, colorB);
 
-    // // Wait some time to connect to wifi
-    // for(int i = 0; i < 10 && WiFi.status() != WL_CONNECTED; i++) {
-    //     Serial.print(".");
-    //     delay(1000);
-    // }
+  //pin led
+  pinMode(0, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(4, OUTPUT); //not use
+  pinMode(5, OUTPUT);
+  pinMode(12, OUTPUT); //not use
+  pinMode(14, OUTPUT);
+  pinMode(16, OUTPUT);
+  // Connect to wifi
+  WiFi.begin(ssid, password);
 
-    // // Check if connected to wifi
-    // if(WiFi.status() != WL_CONNECTED) {
-    //     Serial.println("No Wifi!");
-    //     return;
-    // }
+  // Wait some time to connect to wifi
+  for(int i = 0; i < 10 && WiFi.status() != WL_CONNECTED; i++) {
+      Serial.print(".");
+      delay(1000);
+  }
 
-    // Serial.println("Connected to Wifi, Connecting to server.");
-    // // try to connect to Websockets server
-    // bool connected = client.connect(websockets_server_host);
-    // if(connected) {
-    //     Serial.println("Connecetd!");
-    //     client.send("Hello Server");
-    // } else {
-    //     Serial.println("Not Connected!");
-    // }
-    
-    // // run callback when messages are received
-    // client.onMessage([&](WebsocketsMessage message) {
-    //   Serial.print("Got Message: ");
-    //   Serial.println(message.data());
-    //   if (message.data() == "undefined;16;0") {
-    //     Serial.println(0);
-    //     digitalWrite(16, LOW);
-    //   }
-    //   if (message.data() == "undefined;16;1") {
-    //     Serial.println(1);
-    //     digitalWrite(16, HIGH);
-    //   }
-    // });
+  // Check if connected to wifi
+  if(WiFi.status() != WL_CONNECTED) {
+    lcd.begin(16, 2);
+    lcd.setRGB(colorR, colorG, colorB);
+    Serial.println("No Wifi!");
+    lcd.print("No Wifi!");
+    return;
+  }
+
+  lcd.begin(16, 2);
+  lcd.setRGB(colorR, colorG, colorB);
+  lcd.print("WIFI Connected");
+  Serial.println("Connected to Wifi, Connecting to server.");
+  // try to connect to Websockets server
+  connectWebSocket();
 }
 
 void loop() {
-  giatri = digitalRead(camBienRung);
-  Serial.print("rung: ");
-  Serial.println(giatri);
-  delay(200);
-    // let the websockets client check for incoming messages
-    // if(client.available()) {
-    //     client.poll();
-    // }
-    // delay(500);
+  //let the websockets client check for incoming messages
+  if(client.available()) {
+      client.poll();
+  }
+
+  delay(500);
+
+  // // run callback when messages are received
+  onMessage();
+}
+
+void connectWebSocket(){
+  bool connected = client.connect(websockets_server_host);
+  if(connected) {
+      Serial.println("Connected!");
+      lcd.setCursor(0, 1);
+      lcd.print("              ");
+      lcd.setCursor(0, 1);
+      lcd.print("WS Connected");
+  } else {
+      Serial.println("Not Connected!");
+  }  
+}
+
+void onMessage(){
+  client.onMessage([&](WebsocketsMessage message) {
+    Serial.print("Got Message: ");
+    Serial.println(message.data());
+
+      //led 0
+      if (message.data() == "642d3cd0435ca36eda05177d;0;0"){
+        Serial.println(0);
+        digitalWrite(0, LOW);
+        lcd.setCursor(0, 1);
+        lcd.print("              ");
+        lcd.setCursor(0, 1);
+        lcd.print("0: OFF");
+      }else if (message.data() == "642d3cd0435ca36eda05177d;0;1"){
+        Serial.println(1);
+        digitalWrite(0, HIGH);
+        lcd.setCursor(0, 1);
+        lcd.print("              ");
+        lcd.setCursor(0, 1);
+        lcd.print("0: ON");
+      }else if (message.data() == "642d3cd0435ca36eda05177d;2;0"){  //led 2
+        Serial.println(0);
+        digitalWrite(2, LOW);
+        lcd.setCursor(0, 1);
+        lcd.print("              ");
+        lcd.setCursor(0, 1);
+        lcd.print("2: OFF");
+      }else if (message.data() == "642d3cd0435ca36eda05177d;2;1"){
+        Serial.println(1);
+        digitalWrite(2, HIGH);
+        lcd.setCursor(0, 1);
+        lcd.print("              ");
+        lcd.setCursor(0, 1);
+        lcd.print("2: ON");
+      } else if (message.data() == "642d3cd0435ca36eda05177d;4;0"){ //led 4
+        Serial.println(0);
+        digitalWrite(4, LOW);
+        lcd.setCursor(0, 1);
+        lcd.print("              ");
+        lcd.setCursor(0, 1);
+        lcd.print("4: OFF");
+      } else if (message.data() == "642d3cd0435ca36eda05177d;4;1"){
+        Serial.println(1);
+        digitalWrite(4, HIGH);
+        lcd.setCursor(0, 1);
+        lcd.print("4: ON");
+      } else if (message.data() == "642d3cd0435ca36eda05177d;5;0"){ //led 5
+        Serial.println(0);
+        digitalWrite(5, LOW);
+        lcd.setCursor(0, 1);
+        lcd.print("              ");
+        lcd.setCursor(0, 1);
+        lcd.print("5: OFF");
+      } else if (message.data() == "642d3cd0435ca36eda05177d;5;1"){
+        Serial.println(1);
+        digitalWrite(5, HIGH);
+        lcd.setCursor(0, 1);
+        lcd.print("              ");
+        lcd.setCursor(0, 1);
+        lcd.print("5: ON");
+      } else if (message.data() == "642d3cd0435ca36eda05177d;12;0"){ //led 12
+        Serial.println(0);
+        digitalWrite(12, LOW);
+        lcd.setCursor(0, 1);
+        lcd.print("              ");
+        lcd.setCursor(0, 1);
+        lcd.print("12: OFF");
+      } else if (message.data() == "642d3cd0435ca36eda05177d;12;1"){ 
+        Serial.println(1);
+        digitalWrite(12, HIGH);
+        lcd.setCursor(0, 1);
+        lcd.print("              ");
+        lcd.setCursor(0, 1);
+        lcd.print("12: ON");
+      } else if (message.data() == "642d3cd0435ca36eda05177d;14;0"){ //led 14
+        Serial.println(0);
+        digitalWrite(14, LOW);
+        lcd.setCursor(0, 1);
+        lcd.print("              ");
+        lcd.setCursor(0, 1);
+        lcd.print("14: OFF");
+      } else if (message.data() == "642d3cd0435ca36eda05177d;14;1"){
+        Serial.println(1);
+        digitalWrite(14, HIGH);
+        lcd.setCursor(0, 1);
+        lcd.print("              ");
+        lcd.setCursor(0, 1);
+        lcd.print("14: ON");
+      } else if (message.data() == "642d3cd0435ca36eda05177d;16;0"){ //led 16
+        Serial.println(0);
+        digitalWrite(16, LOW);
+        lcd.setCursor(0, 1);
+        lcd.print("              ");
+        lcd.setCursor(0, 1);
+        lcd.print("16: OFF");
+      } else if (message.data() == "642d3cd0435ca36eda05177d;16;1"){
+        Serial.println(1);
+        digitalWrite(16, HIGH);
+        lcd.setCursor(0, 1);
+        lcd.print("              ");
+        lcd.setCursor(0, 1);
+        lcd.print("16: ON");
+      }
+  });
 }
